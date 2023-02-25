0b4ecb8a101b91383098a0a643b3d5c99e428a7f
jsduniya <jsduniya747@gmail.com>
2023-02-25 19:57:34 +0530

    Added new react aplication and registered with root config

--- src/org-root-config.js
+++ src/org-root-config.js
@@ -1,19 +1,19 @@
 import { registerApplication, start } from "single-spa";
 
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"],
-});
-
 // registerApplication({
-//   name: "@org/navbar",
-//   app: () => System.import("@org/navbar"),
-//   activeWhen: ["/"]
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
 // });
+
+registerApplication({
+  name: "@org/myreact",
+  app: () => System.import("@org/myreact"),
+  activeWhen: ["/"]
+});
 
 start({
   urlRerouteOnly: true,
